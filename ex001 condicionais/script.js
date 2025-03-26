4cf0222f734a9a66996cf7589a19c05bdc23aeae
LucasSantos96 <114953327+LucasSantos96@users.noreply.github.com>
2025-03-25 22:36:10 -0300

    Primeiro commit

---
+++ ex001 condicionais/script.js
@@ -0,0 +1,22 @@
+let nome = prompt("Olá piloto, qual é o seu nome?");
+let velocity = 0;
+let newVelocity = prompt("Olá " + nome + ", a velocidade inicial da sua nave é de " + velocity + " km/s, qual velocidade gostaria de acelerar a nave?");
+let confirmVelocity = confirm("Você confirma a velocidade de " + newVelocity + " km/s?");
+
+(confirmVelocity) ? alert("Velocidade confirmada com sucesso, você esta a"+newVelocity+"km/s") : alert("Cancelar decolagem, velocidade não confirmada");
+
+if(newVelocity <= 0){
+    alert("Nave parada, impossível decolar");
+} if(newVelocity < 40){
+    alert("Você esta devagar, podemos aumentar mais");
+} if(newVelocity >=40 && newVelocity <80){
+    alert("Parece uma boa velocidade para manter");
+}
+if(newVelocity >=80 && newVelocity <100){
+    alert("Velocidade alta, considere diminuir");
+ } if(newVelocity >=100){
+    alert("Velocidade perigosa, controle automático forçado");
+ }
+if(newVelocity >=80 && newVelocity <100){
+    alert("Velocidade alta, considere diminuir");
+}
